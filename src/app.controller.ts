f133c97d94e2e38ff6717945002811da328729a3
Artem <2urnix@gmail.com>
2018-02-20 15:56:17 +0300

    feat(app.controller.ts): mock root endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import {Get, Controller, Param, Req} from '@nestjs/common';
+import {Controller, Get, Query} from '@nestjs/common';
+import {isNumber} from "util";
+
 
 @Controller()
 export class AppController {
-	@Get()
-	root(@Param() params): string {
-    let p = params;
-    console.log(`p: ${JSON.stringify(p)}`);
-    // request.params[param]
-    return 'Hello World!';
+  @Get()
+  root(@Query() params): number | string {
+    try {
+      let value = params.value;
+      console.log(`value: ${JSON.stringify(value)}`);
+      let result = value * 67.50;
+      console.log(`result: ${JSON.stringify(result)}`);
+      console.log(`isNumber(result): ${JSON.stringify(isNumber(result))}`);
+      console.log(`result !== null: ${JSON.stringify(result !== null)}`);
+      if (isNumber(result) && result !== null && result !== undefined && !isNaN(result)) {
+        return result;
+      } else {
+        throw new Error('Not a number');
+      }
+    } catch (error) {
+      console.error(error);
+      return 'Something went wrong';
+    }
   }
 }

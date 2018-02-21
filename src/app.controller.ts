0461491fd6b8ede9fb8b27e02816c318be0cf78a
Artem <2urnix@gmail.com>
2018-02-21 14:42:01 +0300

    feat(app.controller.ts): implement `rates` endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,45 @@
 import {Controller, Get, Query} from '@nestjs/common';
-import {isNumber} from "util";
+import {isNumber} from 'util';
 
+let fetch = require('node-fetch');
+
+const CURRENCIES = ['RUB', 'USD', 'EUR'];
 
 @Controller()
 export class AppController {
-  @Get()
-  root(@Query() params): number | string {
-    try {
-      let value = params.value;
-      console.log(`value: ${JSON.stringify(value)}`);
-      let result = value * 67.50;
-      if (isNumber(result) && result !== null && result !== undefined && !isNaN(result)) {
-        return result;
-      } else {
-        throw new Error('Not a number');
-      }
-    } catch (error) {
-      console.error(error);
-      return 'Something went wrong';
+    @Get()
+    root(@Query() params): number | string {
+        try {
+            let value = params.value;
+            console.log(`value: ${JSON.stringify(value)}`);
+            let result = value * 67.50;
+            if (isNumber(result) && result !== null && result !== undefined && !isNaN(result)) {
+                return result;
+            } else {
+                throw new Error('Not a number');
+            }
+        } catch (error) {
+            // console.error(error);
+            console.log('ERROR');
+            return 'Something went wrong';
+        }
     }
-  }
+
+    @Get('rates')
+    async rates(): Promise<object | string> {
+        try {
+            let result = {};
+            for (let currency of CURRENCIES) {
+                let rest = CURRENCIES.filter(item => item !== currency).join();
+                const response = await fetch(`https://api.fixer.io/latest?base=${currency}&symbols=${rest}`);
+                const json = await response.json();
+                let rates = json.rates;
+                result[currency] = rates;
+            }
+            return result;
+        } catch (error) {
+            console.error(error);
+            return 'Something went wrong';
+        }
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import {ApplicationModule} from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(ApplicationModule);
-    await app.listen(process.env.PORT || 5000)
+    await app.listen(process.env.PORT || 3000)
 }
 
 bootstrap();
 
 let http = require('http');
-if ((new Date()).getHours() > 6) {
+if ((new Date()).getHours() > 3 && (new Date()).getHours() < 21) {
     setInterval(function () {
         http.get('http://falcon-converter.herokuapp.com?value=1&comment=just%20for%20wake%20up');
-    }, 300001);
+    }, 300000);
 }
